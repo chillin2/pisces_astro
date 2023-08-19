f5ed8597050f6f94970c16db9af60bbe31434a02
pisces <chillin2@naver.com>
2023-08-19 16:24:17 +0900

    edit lsp config .clang-format

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
 }
